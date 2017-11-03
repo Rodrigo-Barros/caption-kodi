f76eaf0ab2f744fcd08eec2da0435cfb6c02125b
Rodrigo Gabriel <rodrigogenio12@gmail.com>
2017-11-03 20:41:04 -0200

    success

--- subtube.py
+++ subtube.py
@@ -22,16 +22,16 @@ gd          Scots Gaelic \n
 sr          Serbian \n
 '''
 
-# lang = raw_input("selecione um idioma para as legendas:")
-# videoID = 'Z4Kz50LFiZk'
+lang = raw_input("selecione um idioma para as legendas:")
+videoID = 'hlovDI8K8Ec'
 
-# req = urllib2.Request('http://video.google.com/timedtext?lang=%s&v=%s' %(lang,videoID))
-# response = urllib2.urlopen(req)
-# the_page = response.read()
-# print the_page
-# file=open("sub.xml","w+")
-# file.write(the_page)
-# file.close()
+req = urllib2.Request('http://video.google.com/timedtext?lang=%s&v=%s' %(lang,videoID))
+response = urllib2.urlopen(req)
+the_page = response.read()
+print the_page
+file=open("sub.xml","w+")
+file.write(the_page)
+file.close()
 
 #------------------------importacao das legendas em xml-----------------------------------------
 a=[]
@@ -63,7 +63,8 @@ for tempo in range(len(a)):
 	elif(len(end_mili)==1):
 		end_mili=end_mili+'00'
 
-	
+
+	#only seconds
 	if(start<60 and end<60):
 		if(start<10):
 			start=str(int(start))
@@ -71,20 +72,32 @@ for tempo in range(len(a)):
 		if(end<10):
 			end=str(int(end))
 			end="0"+end
-		print ("start: 00:00:%.2s,%s --> end: 00:00:%.2s,%s" %(start,start_mili,end,int(end_mili)))
+		print ("start: 00:00:%.2s,%s --> 00:00:%.2s,%s" %(start,start_mili,end,int(end_mili)))
+		subFile.write('''%i\n00:00:%.2s,%s --> 00:00:%.2s,%s\n%s\n\n''' % (index,start,start_mili,end,int(end_mili),legenda.encode('utf-8')))
 	elif(start>60 and end>60):
 		#minutes and seconds
-		if(start%60 >= 1):
+		if(start/60 >= 1):
 			start_min = (start/60)
-			start_sec = ((start/60) - int(start_min))*60
+			start_sec = int(((start/60) - int(start_min))*60)
+			if(start_sec<10):
+				start_sec=str(start_sec)
+				start_sec="0"+start_sec
+			if(start_min<10):
+				start_min=str(start_min)
+				start_min="0"+start_min
 		if(end%60 >= 1):
-			start_min = (start/60)
-			start_sec = ((start/60) - int(start_min))*60
-		#print ("start: 00:%.0f:%.0f,%s" %(start_min,start_sec,start_mili))
-		print ("start: 00:%.0f:%.0f,%s" %(start_min,start_sec,start_mili))
+			end_min = (end/60)
+			end_sec = ((end/60) - int(end_min))*60
+			if(end_sec<10):
+				end_sec=str(end_sec)
+				end_sec="0"+end_sec
+			if(end_min<10):
+				end_min=str(end_min)
+				end_min="0"+end_min
+		#print ("end: 00:%.2s:%.2s,%s" %(end_min,end_sec,end_mili))
+		print ("00:%.2s:%.2s,%s --> 00:%.2s:%.2s,%s" %(start_min,start_sec,start_mili,end_min,end_sec,end_mili))
+		subFile.write('''%i\n00:%.2s:%.2s,%s --> 00:%.2s:%.2s,%s \n%s\n\n''' % (index,start_min,start_sec,start_mili,end_min,end_sec,end_mili,legenda.encode('utf-8')))
+ 	#subFile.write('''%i\n:%.0f,%s --> %.0f,%s\n%s\n\n''' % (index, start,start_mili,float(end),end_mili,legenda.encode('utf-8')))
+ 	index+=1
 
-
-# 	subFile.write('''%i\n:%.0f,%s --> %.0f,%s\n%s\n\n''' % (index, start,start_mili,float(end),end_mili,legenda.encode('utf-8')))
-# 	index+=1
-
-# subFile.close()
+subFile.close()
